74794060f989bc8daa875b42a01a3228810257ec
Sparsh1212 <sparshagrawal1212@gmail.com>
2021-04-02 12:43:15 +0530

    Fix #3: Add Splash Page

---
+++ lib/pages/splash.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:bugheist/pages/issues.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => PaginatedClass())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'logo.png',
+              width: 120.0,
+              height: 120.0,
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Text(
+              'Bugheist',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
