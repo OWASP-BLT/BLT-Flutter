c683336fe9250d7d68d087f7c50adb0c3132e57a
Samuel Rosado <SamuelLRosado@jourrapide.com>
2021-04-02 20:36:42 -0400

    added menubar and finalized navigation appbar header

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
-import 'pages/splash.dart';
+
 import 'package:bugheist/pages/login_signup.dart';
 
 void main() {
-  runApp(FlutterSearch());
-}
-
-class FlutterSearch extends StatefulWidget {
-  @override
-  _FlutterSearchState createState() => _FlutterSearchState();
-}
-
-class _FlutterSearchState extends State<FlutterSearch> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginSignUp(),
-    );
-  }
+  runApp(LoginSignUp());
 }
--- lib/pages/issues.dart
+++ lib/pages/issues.dart
@@ -19,6 +19,16 @@ class PaginatedClassState extends State<PaginatedClass>
   late Future _getObj;
   late String paginatedUrl;
 
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   void initState() {
     paginatedUrl = 'https://www.bugheist.com/api/v1/issues/';
@@ -74,183 +84,230 @@ class PaginatedClassState extends State<PaginatedClass>
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            title: Text('Bugheist Issues'),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Image.asset(
+            'assets/bugheist_logo.png',
+            fit: BoxFit.cover,
+            height: 30,
           ),
-          drawer: Drawer(
-            // Add a ListView to the drawer. This ensures the user can scroll
-            // through the options in the drawer if there isn't enough vertical
-            // space to fit everything.
-            child: ListView(
-              // Important: Remove any padding from the ListView.
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                DrawerHeader(
-                  child: Text('Drawer Header'),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                ),
-                ListTile(
-                  title: Text('Login'),
-                  onTap: () {
-                    // Update the state of the app
-                    // ...
-                    // Then close the drawer
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LoginSignUp(),
-                      ),
-                    );
-                  },
-                ),
-                ListTile(
-                  title: Text('Item 2'),
-                  onTap: () {
-                    // Update the state of the app
-                    // ...
-                    // Then close the drawer
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                // do something
+              },
             ),
-          ),
-          body: FutureBuilder(
-            future: _getObj,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return Center(
-                    child: Text(
-                      'Something went wrong!',
-                      style: TextStyle(fontSize: 18),
+            IconButton(
+              icon: Icon(
+                Icons.portrait_rounded,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                // do something
+              },
+            )
+          ],
+        ),
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Color(0xFFDC4654),
+                ),
+              ),
+              ListTile(
+                title: Text('Login'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginSignUp(),
                     ),
                   );
-                } else if (snapshot.hasData) {
-                  return ListView.builder(
-                      itemCount:
-                          (snapshot.data! as DataModel).results.length + 1,
-                      controller: _scrollController,
-                      itemBuilder: (context, index) {
-                        if (index ==
-                            (snapshot.data! as DataModel).results.length)
-                          return Center(
-                            child: Opacity(
-                              opacity: _loading ? 1.0 : 0.0,
-                              child: CircularProgressIndicator(
-                                valueColor: animationController.drive(
-                                  ColorTween(
-                                    begin: Colors.blueAccent,
-                                    end: Colors.red,
-                                  ),
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: FutureBuilder(
+          future: _getObj,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    'Something went wrong!',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                );
+              } else if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: (snapshot.data! as DataModel).results.length + 1,
+                    controller: _scrollController,
+                    itemBuilder: (context, index) {
+                      if (index == (snapshot.data! as DataModel).results.length)
+                        return Center(
+                          child: Opacity(
+                            opacity: _loading ? 1.0 : 0.0,
+                            child: CircularProgressIndicator(
+                              valueColor: animationController.drive(
+                                ColorTween(
+                                  begin: Colors.blueAccent,
+                                  end: Colors.red,
                                 ),
                               ),
                             ),
-                          );
-                        else
-                          return new GestureDetector(
-                            onTap: () => {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ArticleOnePage(
-                                    issue: (snapshot.data! as DataModel)
-                                        .results[index],
-                                  ),
+                          ),
+                        );
+                      else
+                        return new GestureDetector(
+                          onTap: () => {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ArticleOnePage(
+                                  issue: (snapshot.data! as DataModel)
+                                      .results[index],
                                 ),
-                              )
-                            },
-                            child: Column(
-                              children: [
-                                Card(
-                                  margin: EdgeInsets.all(10),
-                                  elevation: 0,
-                                  clipBehavior: Clip.antiAlias,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.only(
-                                          topRight: Radius.circular(15.0),
-                                          bottomRight: Radius.circular(15.0))),
-                                  child: Container(
-                                    height: 130,
-                                    padding: const EdgeInsets.all(0),
-                                    child: Row(
-                                      children: <Widget>[
-                                        Container(
-                                          width: 200,
-                                          height: 130,
-                                          child: Hero(
-                                            tag: (snapshot.data! as DataModel)
-                                                .results[index]
-                                                .screenshot,
-                                            child: DecoratedBox(
-                                              decoration: BoxDecoration(
-                                                image: DecorationImage(
-                                                  image: NetworkImage(
-                                                    (snapshot.data!
-                                                            as DataModel)
-                                                        .results[index]
-                                                        .screenshot,
-                                                  ),
-                                                  fit: BoxFit.cover,
+                              ),
+                            )
+                          },
+                          child: Column(
+                            children: [
+                              Card(
+                                margin: EdgeInsets.all(10),
+                                elevation: 0,
+                                clipBehavior: Clip.antiAlias,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(15.0),
+                                        bottomRight: Radius.circular(15.0))),
+                                child: Container(
+                                  height: 130,
+                                  padding: const EdgeInsets.all(0),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Container(
+                                        width: 200,
+                                        height: 130,
+                                        child: Hero(
+                                          tag: (snapshot.data! as DataModel)
+                                              .results[index]
+                                              .screenshot,
+                                          child: DecoratedBox(
+                                            decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                image: NetworkImage(
+                                                  (snapshot.data! as DataModel)
+                                                      .results[index]
+                                                      .screenshot,
                                                 ),
+                                                fit: BoxFit.cover,
                                               ),
                                             ),
                                           ),
                                         ),
-                                        Flexible(
-                                          child: Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                              horizontal: 10,
-                                            ),
-                                            child: Column(
-                                              children: <Widget>[
-                                                Row(
-                                                  children: <Widget>[
-                                                    Flexible(
-                                                      child: Text(
-                                                        (snapshot.data!
-                                                                as DataModel)
-                                                            .results[index]
-                                                            .description,
-                                                        overflow:
-                                                            TextOverflow.fade,
-                                                        softWrap: true,
-                                                        style: TextStyle(
-                                                            fontWeight:
-                                                                FontWeight.w600,
-                                                            fontSize: 12),
-                                                      ),
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
+                                      ),
+                                      Flexible(
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 10,
                                           ),
-                                        )
-                                      ],
-                                    ),
+                                          child: Column(
+                                            children: <Widget>[
+                                              Row(
+                                                children: <Widget>[
+                                                  Flexible(
+                                                    child: Text(
+                                                      (snapshot.data!
+                                                              as DataModel)
+                                                          .results[index]
+                                                          .description,
+                                                      overflow:
+                                                          TextOverflow.fade,
+                                                      softWrap: true,
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.w600,
+                                                          fontSize: 12),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                )
-                              ],
-                            ),
-                          );
-                      });
-                }
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                    });
               }
-              return Center(
-                child: CircularProgressIndicator(
-                  valueColor: animationController.drive(
-                    ColorTween(
-                      begin: Colors.blueAccent,
-                      end: Colors.red,
-                    ),
+            }
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: animationController.drive(
+                  ColorTween(
+                    begin: Colors.blueAccent,
+                    end: Colors.red,
                   ),
                 ),
-              );
-            },
-          )),
+              ),
+            );
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bug_report),
+              label: 'Report',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Issues',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.leaderboard),
+              label: 'Leaderboard',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color(0xFFDC4654),
+          onTap: _onItemTapped,
+        ),
+      ),
     );
   }
 }
