09eb4e87ac2611d1322b39ba4c3500fa9fb21fe7
Sparsh1212 <sparshagrawal1212@gmail.com>
2021-04-05 09:39:21 +0530

    Fix #12: Convert issue card to stateless

---
+++ lib/pages/components/issue_intro_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class IssueCard extends StatelessWidget {
+  final String description;
+  final String imageSrc;
+
+  IssueCard({required this.description, required this.imageSrc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          margin: EdgeInsets.all(10),
+          elevation: 0,
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0))),
+          child: Container(
+            height: 130,
+            padding: const EdgeInsets.all(0),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 200,
+                  height: 130,
+                  child: Hero(
+                    tag: imageSrc,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(imageSrc),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                description,
+                                overflow: TextOverflow.fade,
+                                softWrap: true,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 12),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
