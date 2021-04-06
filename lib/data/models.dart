9f91df86867f768a4074e2d2db07ec924bc6d16a
Wendy Wallace <WendyRWallace@PiercingTongue.com>
2021-04-05 20:27:24 -0400

    First take at Integrate leaderboard #16

--- lib/data/models.dart
+++ lib/data/models.dart
@@ -39,3 +39,32 @@ class Results {
         screenshot: parsedJson['screenshot']);
   }
 }
+
+class Leaders {
+  final String id;
+  final int rank;
+  final String user;
+  final int score;
+  final String image;
+  final int title;
+
+  Leaders({
+    required this.id,
+    required this.rank,
+    required this.user,
+    required this.score,
+    required this.image,
+    required this.title,
+  });
+
+  factory Leaders.fromJson(Map<String, dynamic> parsedJson) {
+    return Leaders(
+      id: parsedJson['id'].toString(),
+      rank: parsedJson['rank'] ?? 0,
+      user: parsedJson['User'] ?? "",
+      score: parsedJson['score']['total_score'] ?? 0,
+      image: parsedJson['image']['user_avatar'] ?? "",
+      title: parsedJson['title_type']['title'] ?? 0,
+    );
+  }
+}
