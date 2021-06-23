3e66729135404cad3ac58a70fbd0c4159835c511
ankit2001 <ankitchoudhary202.ac@gmail.com>
2021-06-23 14:27:29 +0530

    Login API call successfully added

--- lib/util/app_url.dart
+++ lib/util/app_url.dart
@@ -2,10 +2,10 @@ class AppUrl {
   static const String liveURL = "https://www.bugheist.com";
   static const String liveBaseURL = "https://www.bugheist.com/api/v1";
   static const String localBaseURL = "http://127.0.0.1:4000/api/v1";
-
+  static const String testBaseURL = "http://127.0.0.1:8000";
   static const String baseURL = liveBaseURL;
-  static const String login = liveURL + "/auth/login/";
-  static const String register = liveURL + "/auth/registration/";
+  static const String login = testBaseURL + "/auth/login/";
+  static const String register = testBaseURL + "/auth/registration/";
   static const String forgotPassword = baseURL + "/forgot-password";
-  static const String user = liveURL + "/auth/user/";
+  static const String user = testBaseURL + "/auth/user/";
 }
