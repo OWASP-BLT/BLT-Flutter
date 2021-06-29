8fa5e776266cd2a61e0b0efa1dc9a0d5a96b328f
Fred Falcon <7475382+fredfalcon@users.noreply.github.com>
2021-06-29 09:52:37 -0400

    update to staging

--- lib/util/app_url.dart
+++ lib/util/app_url.dart
@@ -3,10 +3,11 @@ class AppUrl {
   static const String liveBaseURL = "https://www.bugheist.com/api/v1";
   static const String localBaseURL = "http://127.0.0.1:4000/api/v1";
   static const String testBaseURL = "http://127.0.0.1:8000";
-  static const String liveStagingURL = "https://bugheist-staging.herokuapp.com";
-  static const String baseURL = liveStagingURL;
-  static const String login = liveURL + "/auth/login/";
-  static const String register = liveURL + "/auth/registration/";
-  static const String forgotPassword = baseURL + "/forgot-password";
-  static const String user = liveURL + "/auth/user/";
+  static const String stagingURL = "https://bugheist-staging.herokuapp.com";
+  static const String stagingBaseURL = "https://bugheist-staging.herokuapp.com/api/v1";
+  static const String baseURL = stagingBaseURL;
+  static const String login = stagingURL + "/auth/login/";
+  static const String register = stagingURL + "/auth/registration/";
+  static const String forgotPassword = stagingURL + "/forgot-password";
+  static const String user = stagingURL + "/auth/user/";
 }
