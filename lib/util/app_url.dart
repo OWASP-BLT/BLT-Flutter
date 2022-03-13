b733780b708cb841490f256e553c1482429bece1
fredfalcon <7475382+fredfalcon@users.noreply.github.com>
2022-03-12 21:05:31 -0500

    updating package

--- lib/util/app_url.dart
+++ lib/util/app_url.dart
@@ -4,10 +4,13 @@ class AppUrl {
   static const String localBaseURL = "http://127.0.0.1:4000/api/v1";
   static const String testBaseURL = "http://127.0.0.1:8000";
   static const String stagingURL = "https://bugheist-staging.herokuapp.com";
-  static const String stagingBaseURL = "https://bugheist-staging.herokuapp.com/api/v1";
+  static const String stagingBaseURL =
+      "https://bugheist-staging.herokuapp.com/api/v1";
+
   static const String baseURL = stagingBaseURL;
-  static const String login = stagingURL + "/auth/login/";
-  static const String register = stagingURL + "/auth/registration/";
-  static const String forgotPassword = stagingURL + "/forgot-password";
-  static const String user = stagingURL + "/auth/user/";
+
+  static const String login = baseURL + "/auth/login/";
+  static const String register = baseURL + "/auth/registration/";
+  static const String forgotPassword = baseURL + "/forgot-password";
+  static const String user = baseURL + "/auth/user/";
 }
