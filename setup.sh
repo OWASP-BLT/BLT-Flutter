d4525d9fd231c6237b46792a92eeeae328c4793b
Samuel Rosado <SamuelLRosado@jourrapide.com>
2023-03-06 02:47:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-
 set PRODUCT_BUNDLE_IDENTIFIER="${PRODUCT_BUNDLE_IDENTIFIER:=com.apps.blt}"
 
 sed -i.backup "s/com.apps.blt/$PRODUCT_BUNDLE_IDENTIFIER/g" ios/Runner.xcodeproj/project.pbxproj
-
 sed -i.backup "s/com.apps.blt/$PRODUCT_BUNDLE_IDENTIFIER/g" android/app/build.gradle 
 sed -i.backup "s/com.apps.blt/$PRODUCT_BUNDLE_IDENTIFIER/g" android/app/src/main/AndroidManifest.xml
