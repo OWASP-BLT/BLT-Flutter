b0d00f8e60256a5a8f5210cfb1dc74acffa6a2d3
YashSomalkar <yashsomalkar@gmail.com>
2023-03-08 20:06:41 +0530

    Orientation according to screenSize

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'src/app.dart';
 import 'package:flutter/services.dart';
 // Application's entry point
 void main() {
-    WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]).then((value) => runApp(BLT()));
+  WidgetsFlutterBinding.ensureInitialized();
+  // Check the device's screen size
+  var screenSize = MediaQueryData.fromWindow(WidgetsBinding.instance.window).size;
+  if (screenSize.shortestSide < 600) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]).then((value) => runApp(BLT()));
+  }
   runApp(BLT());
 }
