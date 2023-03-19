a039b4f327c352f620934cd02fd54d0e61244395
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2023-03-19 13:39:07 +0530

    Add receiving shared intent logic

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -15,56 +15,14 @@ class BLT extends StatefulWidget {
 }
 
 class BLTState extends State<BLT> {
-  late StreamSubscription _intentDataStreamSubscription;
-  late List<SharedMediaFile> _sharedFiles;
-  //late String _sharedText;
   final _messengerKey = GlobalKey<ScaffoldMessengerState>();
   @override
   void initState() {
     super.initState();
-    // For sharing images coming from outside the app while the app is in the memory
-    _intentDataStreamSubscription = ReceiveSharingIntent.getMediaStream()
-        .listen((List<SharedMediaFile> value) {
-      setState(() {
-        print(_sharedFiles);
-        _sharedFiles = value;
-      });
-    }, onError: (err) {
-      print("getIntentDataStream error: $err");
-    });
-
-    // For sharing images coming from outside the app while the app is closed
-    ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> value) {
-      setState(() {
-        _sharedFiles = value;
-      });
-    });
-
-    // For sharing or opening urls/text coming from outside the app while the app is in the memory
-    _intentDataStreamSubscription =
-        ReceiveSharingIntent.getTextStream().listen((String value) {
-      setState(() {
-        print('detected a link here 1');
-        print(value);
-        //_sharedText = value;
-      });
-    }, onError: (err) {
-      print("getLinkStream error: $err");
-    });
-
-    // For sharing or opening urls/text coming from outside the app while the app is closed
-    ReceiveSharingIntent.getInitialText().then((value) {
-      setState(() {
-        print('detected text here:');
-        print(value);
-        //_sharedText = value;
-      });
-    });
   }
 
   @override
   void dispose() {
-    _intentDataStreamSubscription.cancel();
     super.dispose();
   }
 
