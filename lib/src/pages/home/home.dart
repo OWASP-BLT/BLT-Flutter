480b2296a04c1a333aece0c9a32a4afdaaf10eb1
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2023-03-19 13:39:07 +0530

    Add receiving shared intent logic

--- lib/src/pages/home/home.dart
+++ lib/src/pages/home/home.dart
@@ -10,6 +10,7 @@ import 'package:blt/src/util/enums/login_type.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 import '../../components/appbar.dart';
 import '../../pages/welcome.dart';
@@ -30,6 +31,7 @@ class Home extends ConsumerStatefulWidget {
 class _HomeState extends ConsumerState<Home> {
   late int _selectedIndex;
   late PageController _pageController;
+  ReportPageDefaults reportPageDefaults = ReportPageDefaults();
 
   void _onItemTapped(int index) {
     setState(
@@ -90,64 +92,64 @@ class _HomeState extends ConsumerState<Home> {
         : SizedBox();
   }
 
-  Widget buildLogOUtDialog(){
+  Widget buildLogOUtDialog() {
     return AlertDialog(
-                title: Text(
-                  'You will be logged out of the app !',
-                   style: GoogleFonts.ubuntu(
-                      textStyle: TextStyle(
-                        color: Color(0xFFDC4654),
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
+      title: Text(
+        'You will be logged out of the app !',
+        style: GoogleFonts.ubuntu(
+          textStyle: TextStyle(
+            color: Color(0xFFDC4654),
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      content: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+                onPressed: () async {
+                  await forgetUser();
+                  await Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) => WelcomePage()),
+                      (Route route) => false);
+                  logout();
+                },
+                style: ButtonStyle(
+                  alignment: Alignment.center,
+                  backgroundColor: MaterialStateProperty.all(Color(0xFFDC4654)),
+                ),
+                child: Text(
+                  "Logout",
+                  style: GoogleFonts.ubuntu(
+                    textStyle: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      fontSize: 15,
                     ),
-                ),
-                content: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextButton(onPressed: () async {
-                          await forgetUser();
-                          await Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(
-                              builder: (context) => WelcomePage()), (Route route) => false);
-                          logout();
-                      },
-                      style: ButtonStyle(
-                        alignment: Alignment.center,
-                        backgroundColor: MaterialStateProperty.all(Color(0xFFDC4654)),
-                      ),
-                      child : Text(
-                        "Logout",
-                          style: GoogleFonts.ubuntu(
-                          textStyle: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 1),
-                          fontSize: 15,
-                          ),
-                        ),
-                      )
-                      ),
-                      TextButton(
-                        onPressed: (){
-                        Navigator.pop(context);
-                      },
-                      style: ButtonStyle(
-                        alignment: Alignment.center,
-                        backgroundColor: MaterialStateProperty.all(Color(0xFF737373)),
-                      ),
-                      child : Text(
-                        "Cancel",
-                          style: GoogleFonts.ubuntu(
-                          textStyle: TextStyle(
-                            color: Color.fromRGBO(255, 255, 255, 1),
-                            fontSize: 15,
-                          ),
-                        ),
-                      )
-                      ),
-                    ],
                   ),
+                )),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ButtonStyle(
+                  alignment: Alignment.center,
+                  backgroundColor: MaterialStateProperty.all(Color(0xFF737373)),
                 ),
-            );
+                child: Text(
+                  "Cancel",
+                  style: GoogleFonts.ubuntu(
+                    textStyle: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      fontSize: 15,
+                    ),
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 
   NetworkImage? buildAvatar() {
@@ -178,10 +180,33 @@ class _HomeState extends ConsumerState<Home> {
       keepPage: true,
     );
     super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      ReportPageDefaults defaults = await receiveSharedIntent();
+      if (defaults.sharedMediaFile != null || defaults.text != null) {
+        setState(() {
+          reportPageDefaults = defaults;
+          _onItemTapped(1);
+        });
+      }
+    });
+  }
+
+  Future<ReportPageDefaults> receiveSharedIntent() async {
+    print("getting");
+    String? sharedValue = await ReceiveSharingIntent.getInitialText();
+    print(sharedValue);
+    List<SharedMediaFile> sharedMedia =
+        await ReceiveSharingIntent.getInitialMedia();
+    return ReportPageDefaults(
+      text: sharedValue,
+      sharedMediaFile: (sharedMedia.length > 0) ? sharedMedia[0] : null,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+
     return WillPopScope(
       onWillPop: () async {
         showGeneralDialog(
@@ -190,10 +215,10 @@ class _HomeState extends ConsumerState<Home> {
           barrierDismissible: true,
           barrierColor: Colors.black.withOpacity(0.5),
           transitionDuration: Duration(milliseconds: 400),
-          pageBuilder: (_, __, ___){
+          pageBuilder: (_, __, ___) {
             return buildLogOUtDialog();
           },
-         );
+        );
         return false;
       },
       child: Scaffold(
@@ -234,8 +259,9 @@ class _HomeState extends ConsumerState<Home> {
                   // Then close the drawer
                   await forgetUser();
                   await Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) => WelcomePage()),
-                      (Route route) => false);
+                      MaterialPageRoute(
+                          builder: (context) => WelcomePage()),
+                          (Route route) => false);
                   await logout();
                 },
               ),
@@ -245,10 +271,7 @@ class _HomeState extends ConsumerState<Home> {
                   // Update the state of the app
                   // ...
                   // Then close the drawer
-                  Navigator.pushNamed(
-                    context,
-                    RouteManager.socialPage
-                  );
+                  Navigator.pushNamed(context, RouteManager.socialPage);
                 },
               ),
               ListTile(
@@ -307,8 +330,8 @@ class _HomeState extends ConsumerState<Home> {
                         ),
                       ),
                       style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                        shape: MaterialStateProperty.all<
+                            RoundedRectangleBorder>(
                           RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(8.0),
                           ),
@@ -335,7 +358,9 @@ class _HomeState extends ConsumerState<Home> {
             children: [
               // Feed(),
               IssuesPage(),
-              ReportBug(),
+              ReportBug(
+                reportPageDefaults: reportPageDefaults,
+              ),
               StartHuntPage(),
               LeaderBoard(),
             ]),
@@ -369,6 +394,202 @@ class _HomeState extends ConsumerState<Home> {
         ),
       ),
     );
+    return FutureBuilder<ReportPageDefaults>(
+        future: receiveSharedIntent(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            ReportPageDefaults reportPageDefaults = snapshot.data!;
+            return WillPopScope(
+              onWillPop: () async {
+                showGeneralDialog(
+                  context: context,
+                  barrierLabel: "Barrier",
+                  barrierDismissible: true,
+                  barrierColor: Colors.black.withOpacity(0.5),
+                  transitionDuration: Duration(milliseconds: 400),
+                  pageBuilder: (_, __, ___) {
+                    return buildLogOUtDialog();
+                  },
+                );
+                return false;
+              },
+              child: Scaffold(
+                appBar: buildAppBar(context: context),
+                drawer: Drawer(
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: <Widget>[
+                      UserAccountsDrawerHeader(
+                        decoration: BoxDecoration(
+                          color: Color(0xFFDC4654),
+                        ),
+                        accountName: Text(
+                          currentUser!.username!,
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        accountEmail: Text(
+                          "",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        currentAccountPicture: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          foregroundImage: buildAvatar(),
+                          child: Text(
+                            currentUser!.username!.substring(0, 2),
+                            style: TextStyle(fontSize: 40.0),
+                          ),
+                          foregroundColor: Color(0xFFDC4654),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(buildLogoutText()),
+                        onTap: () async {
+                          // Update trhe state of the app
+                          // ...
+                          // Then close the drawer
+                          await forgetUser();
+                          await Navigator.of(context).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                  builder: (context) => WelcomePage()),
+                              (Route route) => false);
+                          await logout();
+                        },
+                      ),
+                      ListTile(
+                        title: Text('Social'),
+                        onTap: () {
+                          // Update the state of the app
+                          // ...
+                          // Then close the drawer
+                          Navigator.pushNamed(context, RouteManager.socialPage);
+                        },
+                      ),
+                      ListTile(
+                        title: Text('Terms of Service'),
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            RouteManager.legalPage,
+                          );
+
+                          // Update the state of the app
+                          // ...
+                          // Then close the drawer
+                        },
+                      ),
+                      ListTile(
+                        title: Text('About Us'),
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            RouteManager.aboutPage,
+                          );
+
+                          // Update the state of the app
+                          // ...
+                          // Then close the drawer
+                        },
+                      ),
+                      ListTile(
+                        title: Text('Company Dashboard'),
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            RouteManager.companyDashboardPage,
+                          );
+
+                          // Update the state of the app
+                          // ...
+                          // Then close the drawer
+                        },
+                      ),
+                      buildReferralOption(),
+                      ListTile(
+                        title: Container(
+                          width: double.infinity,
+                          height: 50,
+                          child: Builder(builder: (context) {
+                            return TextButton(
+                              child: Text(
+                                "Start Bug Hunt",
+                                style: GoogleFonts.ubuntu(
+                                  textStyle: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                ),
+                                backgroundColor: MaterialStateProperty.all(
+                                  Color(0xFFDC4654),
+                                ),
+                              ),
+                              onPressed: () async {
+                                startBugHunt(context);
+                              },
+                            );
+                          }),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                body: PageView(
+                    controller: _pageController,
+                    onPageChanged: (index) {
+                      setState(() => _selectedIndex = index);
+                    },
+                    children: [
+                      // Feed(),
+                      IssuesPage(),
+                      ReportBug(
+                        reportPageDefaults: reportPageDefaults,
+                      ),
+                      StartHuntPage(),
+                      LeaderBoard(),
+                    ]),
+                bottomNavigationBar: BottomNavigationBar(
+                  type: BottomNavigationBarType.fixed,
+                  items: const <BottomNavigationBarItem>[
+                    // BottomNavigationBarItem(
+                    //   icon: Icon(Icons.home),
+                    //   label: 'Home',
+                    // ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.list),
+                      label: 'Issues',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.bug_report),
+                      label: 'Report',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.location_searching),
+                      label: 'Bug Hunt',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.leaderboard),
+                      label: 'Leaderboard',
+                    ),
+                  ],
+                  currentIndex: _selectedIndex,
+                  selectedItemColor: Color(0xFFDC4654),
+                  onTap: _onItemTapped,
+                ),
+              ),
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 
   @override
