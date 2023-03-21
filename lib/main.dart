1589d10c581602bfb9b28020a95a776c924659ae
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2023-03-21 23:48:08 +0530

    Added sentry to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
 import 'package:flutter/services.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
 // Application's entry point
-void main() {
-    WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]).then((value) => runApp(BLT()));
-  runApp(BLT());
+
+  const SENTRY_DSN = String.fromEnvironment('SENTRY_DSN');
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = SENTRY_DSN;
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(BLT()),
+  );
 }
