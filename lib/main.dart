b099a4339830a4c8b1f18ec744c6bbd3a46d3cb0
YashSomalkar <yashsomalkar@gmail.com>
2023-03-21 20:40:47 +0530

    init_service

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
+import 'package:blt/src/util/services/init_service.dart';
 import 'package:flutter/material.dart';
 import 'src/app.dart';
 import 'package:flutter/services.dart';
 // Application's entry point
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  // Check the device's screen size
-  var screenSize = MediaQueryData.fromWindow(WidgetsBinding.instance.window).size;
-  if (screenSize.shortestSide < 600) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]).then((value) => runApp(BLT()));
-  }
+  InitService.init(); // Call the init method from the InitService class
   runApp(BLT());
 }
