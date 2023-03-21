723b3441075d556631e9a411f1554c9079271d08
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2023-03-22 01:10:06 +0530

    Added a sample DSN for sentry, and overwrite logic in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set PRODUCT_BUNDLE_IDENTIFIER="${PRODUCT_BUNDLE_IDENTIFIER:=com.apps.blt}"
 set BASE_URL="${BASE_URL:=www.owasp.org/BLT/}"
+set SENTRY_DSN="${SENTRY_DSN:=https://example-234234324.com/4504877879197696}"
 
 sed -i.backup "s~www.owasp.org/BLT/~$BASE_URL~g" lib/src/util/endpoints/general_endpoints.dart
+sed -i.backup "s~https://example-234234324.com/4504877879197696~$SENTRY_DSN~g" lib/main.dart
 sed -i.backup "s/com.apps.blt/$PRODUCT_BUNDLE_IDENTIFIER/g" ios/Runner.xcodeproj/project.pbxproj
 sed -i.backup "s/com.apps.blt/$PRODUCT_BUNDLE_IDENTIFIER/g" android/app/build.gradle 
 sed -i.backup "s/com.apps.blt/$PRODUCT_BUNDLE_IDENTIFIER/g" android/app/src/main/AndroidManifest.xml
