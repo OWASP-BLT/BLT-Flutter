c24870eedecb8d7fbd9b216890f6c2e25e2c76cc
Yashsomalkar <95977604+Yashsomalkar@users.noreply.github.com>
2023-04-04 03:20:06 +0530

    fixed the IOS build failed Error

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 // Application's entry point
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   InitService.init(); // Call the init method from the InitService class
   const SENTRY_DSN = "https://example-234234324.com/4504877879197696";
