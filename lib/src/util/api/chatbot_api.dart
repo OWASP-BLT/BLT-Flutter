0254586585f7a4388e1485caa17de1509d791d57
Uttkarsh-raj <106571927+Uttkarsh-raj@users.noreply.github.com>
2024-08-09 23:11:27 +0530

    resolved the chat bot not responding issue (#430)

--- lib/src/util/api/chatbot_api.dart
+++ lib/src/util/api/chatbot_api.dart
@@ -7,8 +7,10 @@ class ChatBotApiClient {
 
   static Future<String> getResponse(String query) async {
     try {
+      var link = GeneralEndPoints.baseUrl + "api/chatbot/conversation/";
       var response = await http.post(
-        Uri.parse("${GeneralEndPoints}/api/chatbot/conversation/"),
+        Uri.parse(link),
+        headers: {"parses": "application/json", "renders": "application/json"},
         body: {"question": query},
       );
 
